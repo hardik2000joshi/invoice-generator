399d89caf218e978dd628c4b0f851cf0a3845566
Hardik Joshi <hardik2000joshi@gmail.com>
2025-07-23 15:09:51 +0530

    Delete debit card and credit card page

--- app/Debit-Card/page.tsx
+++
@@ -1,5 +0,0 @@
-import DebitCard from '@/app/component/form/User/debitCard'
-const DebitCardPage = () => {
-    return < DebitCard />
-}
-export default DebitCardPage;
--- app/credit-card/page.tsx
+++
@@ -1,5 +0,0 @@
-import CreditCard from '@/app/component/form/User/creditCard'
-const CreditCardPage = () => {
-    return < CreditCard/>
-}
-export default CreditCardPage;

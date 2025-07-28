d8d5dfb227132950d608d321a97b43eded1847df
Hardik Joshi <hardik2000joshi@gmail.com>
2025-07-28 17:58:35 +0530

    failure callback

--- app/api/paysecure/failure-callback/route.ts
+++ app/api/paysecure/failure-callback/route.ts
@@ -1,5 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import clientPromise from "@/lib/mongodb";
+
 export async function POST(req: Request) {
+    try {
     const body = await req.json();
     console.log("Failure Callback:", body);
-    return new Response("Failure Callback Received", { status: 200 });
+
+    const purchaseId = body?.message?.purchaseId;
+
+    if (!purchaseId) {
+        return NextResponse.json({error: "Missing purchase id"}, {status:400});
+    }
+
+    const client = await clientPromise;
+    const db = client.db("testData");
+
+    // update payment status to 'failed'
+    await db.collection("payments").updateOne(
+        {purchaseId},
+        {$set: {status: "failed"}}
+    );
+
+    return NextResponse.json ({message: "Failure Callback Received"}, { status: 200 });
+    }
+
+    catch (err) {
+        console.error("Failure callback error:", err);
+        return NextResponse.json({error: "Internal Server Error"}, {status:500});
+    }
 }
